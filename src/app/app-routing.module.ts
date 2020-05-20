13ef85dd4c8a8a94e6b92ed7c97062698806e6fc
Alfonso José Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2020-05-19 18:32:45 -0600

    Se agrega componente de contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Se agrega Modulo
+import { ContactosComponent } from './contactos/contactos.component'
 
-const routes: Routes = [];
+
+// Por útlimo, modificar el arreglo de las rutas ...
+const routes: Routes = [
+
+
+  { path: 'contactos', component: ContactosComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
