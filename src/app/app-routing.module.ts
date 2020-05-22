d9bd4071286c0571cf7d26402165ffe7cdde2e2d
Alfonso José Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2020-05-21 19:11:24 -0600

    Se agrega enlace a modulo de Contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Se agrega Modulo
 import { ContactosComponent } from './contactos/contactos.component'
-
+import { ServiciosComponent } from './servicios/servicios.component';
 
 // Por útlimo, modificar el arreglo de las rutas ...
 const routes: Routes = [
 
-
-  { path: 'contactos', component: ContactosComponent}
-
+  { path: 'contactos', component: ContactosComponent }, 
+  { path: 'servicios', component: ServiciosComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactosComponent } from './contactos/contactos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactosComponent
+    ContactosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
